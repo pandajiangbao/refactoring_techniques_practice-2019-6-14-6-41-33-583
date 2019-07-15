53c45e88ac0b6e4a5bb3561ad8584c1000ac448c
WUPA2 <panda.wu@oocl.com>
2019-07-15 18:16:56 +0800

    refactor OwingPrinter

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,8 +2,9 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
+        boolean onMac = platform.toUpperCase().contains("MAC");
+        boolean isIe = browser.toUpperCase().contains("IE");
+        if (onMac && isIe) {
             return "IE on Mac?";
         }
         return "banner";
--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,12 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+    private static final Integer QUANTITY_MAX=500;
+    private static final Double DISCOUNT_RATE=0.05;
     double getPrice(int quantity, int itemPrice) {
-        // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+        double basePrice = quantity * itemPrice;
+        double discount = Math.max(0, quantity - QUANTITY_MAX) * itemPrice * DISCOUNT_RATE;
+        double shippingCost = Math.min(quantity * itemPrice * 0.1, 100.0);
+        return basePrice - discount + shippingCost;
     }
 }
